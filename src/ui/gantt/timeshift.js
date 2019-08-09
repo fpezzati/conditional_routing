a13b94fcbb879fa8148f38f477e84c5bfeb168dd
Francesco Pezzati <PEFR@maior.local>
2019-08-09 15:17:17 +0200

    Style is the key :)

--- src/ui/gantt/timeshift.js
+++ src/ui/gantt/timeshift.js
@@ -1,7 +1,7 @@
 var TimeShift = (function(){
   return {
     view: function(vnode) {
-      return m("div", { class: "timeshift", top: vnode.attrs.y+"px" , left: (vnode.attrs.x + vnode.attrs.lasts)+"px" }, "shift");
+      return m("div", { class: "timeshift", style: {top: vnode.attrs.y+"px" , left: (vnode.attrs.x + vnode.attrs.lasts)+"px" } }, "shift");
     }
   }
 })();
